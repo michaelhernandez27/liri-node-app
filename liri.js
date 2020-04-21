1273903f15f0d047105f74cf028365a3156f351b
Michael Hernandez <michael.hernandez27@outlook.com>
2020-04-21 17:32:03 -0400

    edited bandsintown functioning and spotify functioning

--- liri.js
+++ liri.js
@@ -1,4 +1,84 @@
 require("dotenv").config(); 
 
 var keys = require('./keys.js');
+var moment = require("moment");
+var fs = require("fs");
+
+
 var Spotify = require ('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+var omdb = (keys.omdb);
+var bandsintown = (keys.bandsintown);
+
+
+var userInput = process.argv[2];
+var userQuery = process.argv.slice(3).join(" ");
+
+function userCommand(userInput, userQuery) {
+    switch (userInput) {
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this":
+            spotifyThisSong();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-this":
+            doThis(userQuery);
+            break;
+        default:
+            console.log("I don't understand");
+            break;
+    }
+}
+
+userCommand(userInput, userQuery);
+
+function concertThis() {
+    console.log(`\n - - - - -\n\nSEARCHING FOR...${userQuery}'s next show...`);
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp" + bandsintown, function (error, response, body) {
+        
+        if (!error && response.statusCode === 200) {
+            
+            var userBand = JSON.parse(body);
+            
+            if (userBand.length > 0) {
+                for (i = 0; i < 1; i++) {
+
+                    console.log(`\nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name}\nVenue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}\nVenue City: ${userBand[i].venue.city}, ${userBand[i].venue.country}`)
+
+                    var concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
+                    console.log(`Date and Time: ${concertDate}\n\n- - - - -`);
+                };
+            } else {
+                console.log('Band or concert not found!');
+            };
+        };
+    });
+};
+
+function spotifyThisSong() {
+    console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
+
+    if (!userQuery) {
+        userQuery = "the sign ace of base"
+    };
+
+    spotify.search({
+        type: 'track',
+        query: userQuery,
+        limit: 1
+    }, function (error, data) {
+        if (error) {
+            return console.log('Error occurred: ' + error);
+        }
+        let spotifyArr = data.tracks.items;
+
+        for (i = 0; i < spotifyArr.length; i++) {
+            console.log(`\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n - - - - -`)
+        };
+    });
+}
